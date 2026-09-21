fb6863b54e375d4c73781c67405173618fbecf3b
ederartmo <121074645+ederartmo@users.noreply.github.com>
2026-09-21 11:12:44 -0600

    Route available BIB listing through consolidated API

--- api/data.js
+++ api/data.js
@@ -8,6 +8,7 @@
 const ACTIONS = {
   'me-registrations': require('../lib/me-registrations'),
   'admin-list-inscriptions': require('../lib/admin-list-inscriptions'),
+  'admin-list-available-bibs': require('../lib/admin-list-available-bibs'),
   'admin-cancel-registration': require('../lib/admin-cancel-registration'),
   'admin-archive-attempt': require('../lib/admin-archive-attempt'),
   'resend-emails-list': require('../lib/resend-emails-list'),
